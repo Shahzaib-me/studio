4b0619d2e98fe41deb925cf9a7a417589b3b053b
Rana Shahzaib <info.shahzaibrajput@gmail.com>
2026-01-06 03:06:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1501439770-a1244",
+  "appId": "1:1038521665200:web:aadbc6d07fe6527d0ddd75",
+  "apiKey": "AIzaSyBxuD55MWXbCz4qeMP3eG7Be3qxG3QrAYg",
+  "authDomain": "studio-1501439770-a1244.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1038521665200"
+};
